882d5ca7965509ee989e1446c29785ee6a1d1e64
Prabhjot Singh <prabhjotassi16@gmail.com>
2025-11-26 22:34:03 +0530

    Add Vercel Analytics integration and update package dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import ErrorBoundary from "@/components/ErrorBoundary";
+import { Analytics } from "@vercel/analytics/react";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -31,6 +32,7 @@ const App = () => (
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
+        <Analytics />
       </ThemeProvider>
     </QueryClientProvider>
   </ErrorBoundary>
